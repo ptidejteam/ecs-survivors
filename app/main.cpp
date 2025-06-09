6311d6ee0fba3bfd5fc17c53f4e94414e35cfa09
Laurent Voisard <laurentvoisard1@gmail.com>
2025-06-09 07:16:56 -0400

    smoll changes

--- app/main.cpp
+++ app/main.cpp
@@ -4,16 +4,12 @@
 
 int main() {
 #ifdef EMSCRIPTEN
-    const int screenWidth = 1440;
-    const int screenHeight = 810;
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
 #else
     const int screenWidth = 1920;
     const int screenHeight = 1080;
 #endif
-    //tmx::Map map;
-    // if(map.load("../resources/tiled/maps/sampleMap.tmx")) {
-    //     std::cout << "Map loaded" << std::endl;
-    // }
     Game("ECS-Survivors", screenWidth, screenHeight).run();
 
     return 0;
